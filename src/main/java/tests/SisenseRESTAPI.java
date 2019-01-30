01cea643760e483337a4ca58444f3ef154dc87a5
Kobbi Gal <kgwack@gmail.com>
2019-01-30 15:47:59 +0200

    improved logs for query failures, added response code

--- src/main/java/tests/SisenseRESTAPI.java
+++ src/main/java/tests/SisenseRESTAPI.java
@@ -10,6 +10,7 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONObject;
+import sun.rmi.runtime.Log;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -41,24 +42,34 @@ public class SisenseRESTAPI {
             get.addHeader("authorization", "Bearer " + configFile.getToken());
             HttpResponse response = client.execute(get);
             HttpEntity entity = response.getEntity();
-
+            int responseCode = response.getStatusLine().getStatusCode();
 
             if (entity != null){
+
                 try(InputStream inputStream = entity.getContent()) {
 
                     String result = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
                     logger.write("[queryTableIsSuccessful] Running query: " + query.replaceAll("%20", " "));
-                    JSONObject jsonObject = new JSONObject(result);
-                    int count = jsonObject.getJSONArray("values").getJSONArray(0).getInt(0);
-                    logger.write("[queryTableIsSuccessful] Result: " + count);
 
-                    if (count > 0){
-                        return true;
+                    if (responseCode == 200){
+
+                        JSONObject jsonObject = new JSONObject(result);
+                        int count = jsonObject.getJSONArray("values").getJSONArray(0).getInt(0);
+                        logger.write("[queryTableIsSuccessful] Result: " + count);
+
+                        if (count > 0){
+                            return true;
+                        }
+                    } else {
+                        logger.write("[queryTableIsSuccessful] query table failed.");
+                        logger.write("[queryTableIsSuccessful] called returned " + responseCode);
+                        logger.write("[queryTableIsSuccessful] response: " + result);
+                        return false;
                     }
                 }
             }
         }catch (Exception e){
-            logger.write("[queryTableIsSuccessful] query table failed:\n");
+            logger.write("[queryTableIsSuccessful] query table failed:");
             logger.write(e.getMessage());
             logger.write(Arrays.toString(e.getStackTrace()));
             return false;
